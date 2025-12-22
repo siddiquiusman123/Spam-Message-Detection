8c5831c4cb52f0022b3d7837eb50067f4e05041d
Siddiqui Usman Ahmed Siraj Ahmed <siddiquiusman915256@gmail.com>
2025-12-22 21:30:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 def download_nltk():
+    
     nltk.download("punkt", quiet=True)
+    nltk.download("punkt_tab", quiet=True)
     nltk.download("stopwords", quiet=True)
     nltk.download("wordnet", quiet=True)
     nltk.download("omw-1.4", quiet=True)
